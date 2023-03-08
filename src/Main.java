69c891d9a2323387fd7077c7716add924ad13c26
Vovashprot <vova.protasenko@gmail.com>
2023-03-08 20:33:34 +0300

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.time.LocalDate;
 public class Main {
     public static void leapYear(int year){
         if (((year % 4 == 0) && (year % 100 !=0)) || (year % 400 == 0))
@@ -7,8 +8,32 @@ public class Main {
         else {System.out.println(year + " не високосный год");}
 
     }
+
+    public static void osVersion(int osType, int releaseYear){
+        int currentYear = LocalDate.now().getYear();
+        if (osType == 1){
+            if(releaseYear < currentYear){
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            }
+            else {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+        }
+        if (osType == 0) {
+            if(releaseYear < currentYear){
+                System.out.println("Установите облегченную версию приложения для IOS по ссылке");
+            }
+            else {
+                System.out.println("Установите версию приложения для IOS по ссылке");
+            }
+        }
+
+    }
     public static void main(String[] args) {
+        System.out.println("Task1");
         leapYear(2021);
+        osVersion(1,2019);
+
     }
 
 }
